618dd304e7e4295d30d5eb8aa38ca5542590e42f
Mateo Contreras <macontrerasc@ufpso.edu.co>
2022-06-02 15:52:17 -0500

    AJUSTE/IMPLEMENTACIÓN - Directivas "v-model" y Validación de Celdas
    - Se agregan las directivas "v-model" a los inputs que reciben la información para luego visualizar en la tabla.
    - Se validan que los inputs contengan información antes de enviar.
    - Se limpian los cajas de texto luego de que el registro haya sido exitoso.

--- js/Gestion.js
+++ js/Gestion.js
@@ -2,16 +2,41 @@ var app = new Vue({
     el: "#gestionOrder",
     data: {
         inputDesc: "",
-        inputName: "" ,
-        inputPriority: "",
+        inputName: "",
+        inputPriority: "Seleccione una",
         inputTel: "",
-        inputTypePay: "",
+        inputTypePay: "Seleccione uno",
         inputDir: "",
-        inputCity: "",
+        inputCity: "Seleccione una",
         inputRef: "",
         order: [],
         prioridad: ["Seleccione una", "Alta", "Media", "Baja"],
         tipoPago: ["Seleccione uno", "Contraentregra", "Tarjeta de Crédito", "PSE"],
         ciudades: ["Seleccione una", "Armenia", "Bogotá", "Bucaramanga", "Barranquilla", "Bello", "Cúcuta", "Cali", "Cartagena", "Manizales", "Medellín", "Ocaña", "Pereira", "Santa Marta"]
+    },
+    methods: {
+        validateFields: function () {
+            if (this.inputDesc && this.inputName) {
+                if (this.prioridad.indexOf(this.inputPriority) != 0) {
+                    if (this.inputTel && this.tipoPago.indexOf(this.inputTypePay) != 0) {
+                        if (this.inputDir && this.ciudades.indexOf(this.inputCity) != 0) {
+                            this.clearFields();
+                            return true;
+                        }
+                    }
+                }
+            }
+            return false;
+        },
+        clearFields: function() {
+            this.inputDesc = "";
+            this.inputName = "";
+            this.inputPriority = "Seleccione una";
+            this.inputTel = "";
+            this.inputTypePay = "Seleccione uno";
+            this.inputDir = "";
+            this.inputCity = "Seleccione una";
+            this.inputRef = "";
+        }
     }
 })
